47a3bef34ab497fba324fb2685526c752579d733
Sascha Mücke <sascha.muecke@tu-dortmund.de>
2020-11-09 17:33:19 +0100

    version 0.4

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Evolutionary Algorithm Optimization (EAO)
 
 Package for optimization using Evolutionary Algorithms (EA), providing base classes for individuals and an evaluation module.
+
+
+## Version History
+
+* **0.4** Removed necessity to manually assign `loss_` attribute
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(name='eao',
-      version='0.3',
+      version='0.4',
       description='Evolutionary Optimization',
       long_description=long_description,
       long_description_content_type='text/markdown',
